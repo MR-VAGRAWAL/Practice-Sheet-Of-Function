3336dc9522a7f488a8e4142d270a75ed0fbfdcbb
Vaibhav Agrawal <107701204+MR-VAGRAWAL@users.noreply.github.com>
2022-12-11 23:22:04 +0530

    Function Question 4

--- Ques4.py
+++ Ques4.py
@@ -1,4 +1,4 @@
-#  4. Write a function to tell user if he/she is able to vote or not.
+# 4. Write a function to tell user if he/she is able to vote or not.
 def voter(age):
     if age >= 18:
         return "Yes , You Are Eligible For Voting"
