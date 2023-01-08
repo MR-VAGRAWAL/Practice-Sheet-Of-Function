92a7dbe8fb1dd99b5f9786b2ab51317084061bcf
Vaibhav Agrawal <107701204+MR-VAGRAWAL@users.noreply.github.com>
2023-01-08 23:30:23 +0530

    Function Question 1

--- Ques1.py
+++ Ques1.py
@@ -1,5 +1,5 @@
 #  1. Write a function to calculate area and perimeter of a rectangle.
-area = (lambda length , breadth : length*breadth)
-perimeter = (lambda length , breadth : 2*(length+breadth))
+area = (lambda length , breadth  : length*breadth)
+perimeter = (lambda length , breadth  : 2*(length+breadth))
 print(area(3,7))
 print(perimeter(3,7))
